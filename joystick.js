fbb461d22055e593cb9f528b1eb004b21be2126c
Saint4kt <wb5zhjrzbf@privaterelay.appleid.com>
2026-07-03 11:21:56 -0500

    Añade base del joystick

---
+++ joystick.js
@@ -0,0 +1,14 @@
+const joystick = {
+  baseX: 90,
+  baseY: window.innerHeight - 90,
+  radius: 50,
+  knobRadius: 22,
+
+  active: false,
+
+  knobX: 90,
+  knobY: window.innerHeight - 90,
+
+  dx: 0,
+  dy: 0
+};
